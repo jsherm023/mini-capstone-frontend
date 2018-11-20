789323deaf60e9cb6da5fd316103701b356d3146
“Joe <joseph.sherman@Loop.colum.edu>
2018-11-19 21:01:22 -0600

    Add backend data to frontend. Installed and used Axios to do so. Displayed all products on localhost:8080.

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <p v-for="product in products"> {{product.name}}</p>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require('axios');
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: [],
+    };
+  },
+  created: function() {
+    axios.get('http://localhost:3000/api/products').then(function(response) {
+      console.log(response.data);
+      this.products = response.data;
+      }.bind(this))
+  },
+  methods: {},
+  computed: {}
+};
+</script>
